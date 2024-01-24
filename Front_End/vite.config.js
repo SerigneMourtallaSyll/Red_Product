8156df509a9d1e5e10b1f3809a5a73a90f3b9c7f
Mourtallasyll <serignemourtallasyll86@gmail.com>
2024-01-24 22:39:44 +0000

    Ajout du fichier vercel.json

--- Front_End/vite.config.js
+++ Front_End/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
-import Checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
